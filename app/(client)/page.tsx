fe1321a6074dc8afda60ef7cb1fe978a169ea462
noorjsdivs <noor.jsdivs@gmail.com>
2025-03-20 08:25:33 +0600

    sanity seed data is added

---
+++ app/(client)/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Container className="bg-shop-light-pink">
+      <HomeBanner />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
+    </html>
+  );
+};
+export default RootLayout;
