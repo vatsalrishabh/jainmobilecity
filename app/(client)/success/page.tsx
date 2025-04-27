7ac70ce93e39ab08c07f59426aed491d911b13ee
noorjsdivs <noor.jsdivs@gmail.com>
2025-04-28 02:47:56 +0600

    error resolved

--- app/(client)/success/page.tsx
+++ app/(client)/success/page.tsx
@@ -2,12 +2,12 @@
 
 import useStore from "@/store";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { motion } from "motion/react";
 import { Check, Home, Package, ShoppingBag } from "lucide-react";
 import Link from "next/link";
 
-const SuccessPage = () => {
+const SuccessPageContent = () => {
   const { resetCart } = useStore();
   const searchParams = useSearchParams();
   const orderNumber = searchParams.get("orderNumber");
@@ -73,6 +73,14 @@ const SuccessPage = () => {
         </div>
       </motion.div>
     </div>
+  );
+};
+
+const SuccessPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SuccessPageContent />
+    </Suspense>
   );
 };
 
