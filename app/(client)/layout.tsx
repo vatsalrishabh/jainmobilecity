d666327f7cdacb30afd62552a2964a6372a10af8
VatsalRishabh27 <vatsalrishabh00@gmail.com>
2025-05-24 14:59:09 +0530

    react version depreciated

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -5,10 +5,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - Shopcart online store",
-    default: "Shopcart online store",
+    template: "%s - Jain Mobile City online store",
+    default: "Jain Mobile City online store",
   },
-  description: "Shopcart online store, Your one stop shop for all your needs",
+  description: "Jain Mobile City online store, Your one stop shop for all your needs",
 };
 
 export default function RootLayout({
